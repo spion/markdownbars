4efa0ade59f58b90a9bbf9963d164b2d2b17eca3
David <david@blackang.us>
2016-09-23 15:42:33 +0200

    adding options / updating handlebars
    update handlebars dependance to add helpers support
    add option —strict
    add option —keep-exp

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # handlebars-cmd
 
-Invoke handlebars from the commandline. 
+Invoke handlebars from the commandline.
 
 Example:
 
@@ -10,13 +10,23 @@ Output:
 
     Hello Test
 
-You may also pass a JSON string as an argument and it will be 
+You may also pass a JSON string as an argument and it will be
 interpreted as an object.
 
 
 You can also pass a JSON file:
 
     handlebars file.json < template.hbs > output.txt
+
+# options
+
+To activate handlebars strict mode (will throw error on missing expressions) :
+
+    handlebars file.json --strict < template.hbs > output.txt
+
+To keep missing expressions in output (they will no longer be replaced by empty string) :
+
+    handlebars file.json --keep-exp < template.hbs > output.txt
 
 # install
 
@@ -25,11 +35,11 @@ You can also pass a JSON file:
 # include helper
 
 handlebars-cmd comes with a built-in helper `#include`
-    
+
     {{{include 'api.md'}}}
 
 You can also pass context (optional)
-    
+
     {{{include 'render.md.hbs' item}}}
 
 # license
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
 
-var fs   = require('fs'),
+var fs = require('fs'),
+    path = require('path'),
     args = require('optimist').argv,
-    hbs  = require('handlebars');
+    hbs = require('handlebars'),
+    options = {
+        'strict': (typeof args.strict !== 'undefined' && args.strict)
+    },
+    keepMissingExpressions = (typeof args['keep-missing'] !== 'undefined' && args['keep-missing']);
 
 if (args._.length) {
     try {
-        args = JSON.parse(fs.readFileSync(args._[0]).toString());
-    } catch (e) { }
-}
-else for (var key in args) {
-    try {
-        args[key] = JSON.parse(args[key]);
-    } catch (e) {
+        var configFile = args._[0].toString();
+
+        if (configFile.match(/js$/)) {
+            args = require(path.join(process.cwd(), configFile));
+        } else {
+            args = JSON.parse(fs.readFileSync(configFile).toString());
+        }
+    } catch (e) {}
+} else
+    for (var key in args) {
+        try {
+            args[key] = JSON.parse(args[key]);
+        } catch (e) {}
     }
-}
 
 function readStream(s, done) {
     var bufs = [];
@@ -29,15 +39,19 @@ function readStream(s, done) {
 
 readStream(process.stdin, function(err, tmpl) {
     function handle(tmpl, args) {
-        hbs.registerHelper('include', function (file, context, opt) {
+        hbs.registerHelper('include', function(file, context, opt) {
             var context = null == context ? args : context;
             var f = fs.readFileSync(file);
-            return handle(f, context); 
+            return handle(f, context);
         });
-        var template = hbs.compile(tmpl.toString());
+        if ( keepMissingExpressions ) {
+            hbs.registerHelper('helperMissing', function(arg) {
+                return '{{' + arg.name + '}}';
+            });
+        }
+        var template = hbs.compile(tmpl.toString(), options);
         var result = template(args);
         return result;
     }
     process.stdout.write(handle(tmpl, args));
 });
-
