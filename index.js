c6176f1cdea7cba702babcfd5d0d46564243cb0d
David Babel <david.babel@ogury.co>
2017-10-07 23:14:16 +0200

    reformat everything

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    path = require('path'),
-    args = require('optimist').argv,
-    encode = require('entity-convert'),
-    hbs = require('handlebars'),
-    options = {
-        'strict': (typeof args.strict !== 'undefined' && args.strict),
-        'noEscape': true
-    },
-    keepMissingExpressions = (typeof args['keep-missing'] !== 'undefined' && args['keep-missing']);
+  path = require('path'),
+  args = require('optimist').argv,
+  encode = require('entity-convert'),
+  hbs = require('handlebars'),
+  options = {
+    'strict': (typeof args.strict !== 'undefined' && args.strict),
+    'noEscape': true
+  },
+  keepMissingExpressions = (typeof args['keep-missing'] !== 'undefined' && args['keep-missing']);
 
 
 if (!Object.assign) {
@@ -46,51 +46,57 @@ if (!Object.assign) {
 }
 
 if (args._.length) {
-    try {
-        var configFile = args._[0].toString();
+  try {
+    var configFile = args._[0].toString();
 
-        if (configFile.match(/js$/)) {
-            args = require(path.join(process.cwd(), configFile));
-        } else {
-            var beforeEscape = fs.readFileSync(configFile).toString();
-            // var afterEscape = encode.html(beforeEscape);
-            // args = JSON.parse(afterEscape);
-            args = JSON.parse(beforeEscape);
-        }
-    } catch (e) {}
-} else
-    for (var key in args) {
-        try {
-            args[key] = JSON.parse(args[key]);
-        } catch (e) {}
+    if (configFile.match(/js$/)) {
+      args = require(path.join(process.cwd(), configFile));
+    } else {
+      var beforeEscape = fs.readFileSync(configFile).toString();
+      // var afterEscape = encode.html(beforeEscape);
+      // args = JSON.parse(afterEscape);
+      args = JSON.parse(beforeEscape);
     }
+  } catch (e) { }
+} else
+  for (var key in args) {
+    try {
+      args[key] = JSON.parse(args[key]);
+    } catch (e) { }
+  }
 
 function readStream(s, done) {
-    var bufs = [];
-    s.on('data', function(d) {
-        bufs.push(d);
-    });
-    s.on('end', function() {
-        done(null, Buffer.concat(bufs));
-    });
-    s.resume();
+  var bufs = [];
+  s.on('data', function (d) {
+    bufs.push(d);
+  });
+  s.on('end', function () {
+    done(null, Buffer.concat(bufs));
+  });
+  s.resume();
 }
 
-readStream(process.stdin, function(err, tmpl) {
-    function handle(tmpl, args) {
-        hbs.registerHelper('include', function(file, context, opt) {
-            var context = null == context ? args : Object.assign({}, args, JSON.parse(context));
-            var f = fs.readFileSync(file);
-            return handle(f, context);
-        });
-        if ( keepMissingExpressions ) {
-            hbs.registerHelper('helperMissing', function(arg) {
-                return '{{' + arg.name + '}}';
-            });
-        }
-        var template = hbs.compile(tmpl.toString(), options);
-        var result = template(args);
-        return result;
+readStream(process.stdin, function (err, tmpl) {
+  function handle(tmpl, args) {
+    hbs.registerHelper('include', function (file, context, opt) {
+      if (typeof context === 'string') {
+        context = Object.assign({}, args, JSON.parse(context));
+      } else if (typeof context === 'object') {
+        context = Object.assign({}, args, context);
+      } else {
+        context = args;
+      }
+      var f = fs.readFileSync(file);
+      return handle(f, context);
+    });
+    if (keepMissingExpressions) {
+      hbs.registerHelper('helperMissing', function (arg) {
+        return '{{' + arg.name + '}}';
+      });
     }
-    process.stdout.write(handle(tmpl, args));
+    var template = hbs.compile(tmpl.toString(), options);
+    var result = template(args);
+    return result;
+  }
+  process.stdout.write(handle(tmpl, args));
 });
